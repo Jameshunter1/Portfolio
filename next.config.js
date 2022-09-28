c9fa082323840baf6a8ca3e8a583a1c4971f4f59
[James Hunter] <Jameshunter1017@live.com>
2022-09-28 17:32:37 -0400

    Final Final

--- next.config.js
+++ next.config.js
@@ -7,3 +7,7 @@ const nextConfig = {
 module.exports = nextConfig 
  
 
+module.exports = {
+  images:{
+ domains: ["cdn.sanity.io"]
+}}
--- pages/api/getProjects.ts
+++ pages/api/getProjects.ts
@@ -15,7 +15,7 @@ projects: Project[];
 
 export default async function handler(
     req: NextApiRequest,
-    res:NextApiResponse<Data>
+    res: NextApiResponse<Data>
 ) {
     const projects:Project[] = await sanityClient.fetch(query);
 
