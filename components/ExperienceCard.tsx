a14168075a362dd2b388f1bb428a1d527eec13b5
[James Hunter] <Jameshunter1017@live.com>
2022-12-25 12:29:44 -0500

    Added chevrons for project section

--- components/ExperienceCard.tsx
+++ components/ExperienceCard.tsx
@@ -3,53 +3,61 @@ import Image from 'next/image'
 import React from 'react'
 import { urlFor } from '../sanity'
 import { Experience } from '../typings'
+import 'react-slideshow-image/dist/styles.css'
+
+
 
 type Props = {
     experience: Experience;
 }
 
- function ExperienceCard({ experience }: Props) {
-  return (
-      <article className='flex flex-col rounded-lg items-center space-y-7 flex-shrink-0 w-[500px] h-[700px] md:w-[650px] md:h[400px] xl:w-[900px] xl:h-[700px] snap-center bg-[#292929] hover:opacity-100 opacity-40 cursor-pointer transition-opacity duration-200 overflow-hidden p-10'>
-          <motion.img
-              initial={{
-                  y: -100,
-                  opacity:0,
-              }}
-              transition={{ duration: 1.2 }}
-              whileInView={{ opacity: 1, y: 0 }}
-              viewport={{ once: true}}
-              className='w-32 h-32 rounded-full md:rounded-full object-cover object-center'
-              src={urlFor(experience?.companyImage).url()}
-              alt="company logo"
+function ExperienceCard({ experience }: Props) {
+    return (
+        
+         
+            <article className='flex flex-col rounded-lg items-center space-y-7 flex-shrink-0 w-[500px] h-[700px] md:w-[650px] md:h[400px] xl:w-[900px] xl:h-[700px] snap-center bg-[#292929] hover:opacity-100 opacity-40 cursor-pointer transition-opacity duration-200 overflow-hidden p-10'>
+                <motion.img
+                    initial={{
+                        y: -100,
+                        opacity: 0,
+                    }}
+                    transition={{ duration: 1.2 }}
+                    whileInView={{ opacity: 1, y: 0 }}
+                    viewport={{ once: true }}
+                    className='w-32 h-32 rounded-full md:rounded-full object-cover object-center'
+                    src={urlFor(experience?.companyImage).url()}
+                    alt="company logo"
              
-          />
-          <div className='px-0 md:px-10'>
-              <h4 className='text-4xl font-light '>
-                  {experience?.jobTitle}</h4>
-              <p className='font-bold text-2xl mt-1'>{experience.company}</p>
-              <div className='flex space-x-2 my-2'>
-                  {experience.technologies.map((technology) => (
-                      <Image
-                          key={technology._id}
-                          className='rounded-full'
-                          src={urlFor(technology?.image).url()} alt="technologies used"
-                          height="45"
-                          width="45"
-                      />
-                  ))}             
+                />
+                <div className='px-0 md:px-10'>
+                    <h4 className='text-4xl font-light '>
+                        {experience?.jobTitle}</h4>
+                    <p className='font-bold text-2xl mt-1'>{experience.company}</p>
+                    <div className='flex space-x-2 my-2'>
+                        {experience.technologies.map((technology) => (
+                            <Image
+                                key={technology._id}
+                                className='rounded-full'
+                                src={urlFor(technology?.image).url()} alt="technologies used"
+                                height="45"
+                                width="45"
+                            />
+                        ))}
+                    </div>
+                    <p className='uppercase py-5 text-gray-300'>{new Date(experience.dateStarted).toDateString()}-{""}
+                        {experience.isCurrentlyWorkingHere ? "Present" : new Date(experience.dateEnded).toDateString()}</p>
+                    <ul className='list-disc space-y-4 text-lg h-80 overflow-y-scroll scrollbar-thin scrollbar-thumb-[#5572af]/80'>
+                        {experience.points.map((point, i) => (
+                            <li key={i}>{point}</li>
+                        ))}
+
+                    </ul>
                 </div>
-              <p className='uppercase py-5 text-gray-300'>{new Date(experience.dateStarted).toDateString()}-{""}
-                  {experience.isCurrentlyWorkingHere ? "Present" : new Date (experience.dateEnded).toDateString()}</p>
-              <ul className='list-disc space-y-4 text-lg h-80 overflow-y-scroll scrollbar-thin scrollbar-thumb-[#5572af]/80'>
-                  {experience.points.map((point, i) => (
-                  <li key={i}>{point}</li>
-                  ))}
+            </article>
 
-              </ul>
-          </div>
-   </article>
-  )
+
+        
+    )
 }
-
+   
 export default ExperienceCard;
--- components/Projects.tsx
+++ components/Projects.tsx
@@ -3,66 +3,66 @@ import {motion }from "framer-motion"
 import { Project } from '../typings';
 import { urlFor } from '../sanity';
 import Image from 'next/image';
-
+import { Slide } from 'react-slideshow-image'
+import {useState} from "react"
+import Slider from 'react-slick';
+import 'react-slideshow-image/dist/styles.css';
 type Props = {
 projects:Project[]
 }
 
- function Project({projects }: Props) {
-   
+
+function Project({ projects }: Props) {
+   const settings = {
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  dots: true,
+  // include other desired settings here
+
+};const slider = React.useRef(null);
   return (
-      <motion.div
-          initial={{ opacity: 0 }}
-          whileInView={{ opacity: 1 }}
-          transition={{duration:1.5}}
-          className='h-screen relative flex overflow-hidden flex-col text-left md:flex-row max-w-full justify-evenly mx-auto items-center z-0 '>
-          <h3 className='absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl'>
-              Projects
-          </h3>
-          <div className="relative w-full flex overflow-x-scroll overflow-y-hidden snap-x snap-mandatory z-20 mt-5 scrollbar scrollbar-track-gray-400/20 scrollbar-thumb-[#5572af]/40">
+   <Slide {...settings}> 
+    <motion.div
+      initial={{ opacity: 0 }}
+      whileInView={{ opacity: 1 }}
+      transition={{ duration: 1.5 }}
+      className='h-screen relative flex overflow-hidden flex-col text-left md:flex-row max-w-full justify-evenly mx-auto items-center z-0 '>
+      <h3 className='absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl'>
+        Projects
+      </h3>
+      <div className="relative w-full flex overflow-x-scroll overflow-y-hidden snap-x snap-mandatory z-20 mt-5 scrollbar scrollbar-track-gray-400/20 scrollbar-thumb-[#5572af]/40">
               
-              {/* Maps through the array of projects */}
-              {projects?.map((project, i) => (
-                  <div key={project._id} className=" w-screen flex-shrink-0 snap-center flex flex-col space-y-5 items-center justify-center p-20 md:p-44 h-screen">
-                      <motion.img
-                          initial={{
-                              y: -300,
-                              opacity: 0,
-                          }}
-                          transition={{ duration: 1.2 }}
-                          whileInView={{ opacity: 1, y: 0 }}
-                          viewport={{ once: true }}
-                          src={urlFor(project?.image).url()}
-                          
-                        
-                        
-                         />
-                      <div className='space-y-10 px-0 md:px-10 max-w-6xl'>
-                          <h4 className='text-4xl font-semibold text-center'>
-                              <span className='underline decoration-[#5572af]/50'>Case Study {i+ 1} of {projects.length}: </span>
-                              {""} {project?.title}
-                          </h4>
-
-                          <div className='flex items-center space-x-7 justify-center'>
-                              {project?.technologies.map((technology) => (
-                                  <Image className='h-10 w-10 space-x-2'
-                                    
-                                      key={technology._id} src={urlFor(technology.image).url()} alt=""
-                                      width="40"
-                                  height="40"/>
-                          ))}
-                              </div>
-
-                          <p className='text-lg text-center md:text-left'>{project?.summary}</p>
-                      </div>
-                      </div>
-              ))}
-         
-          </div>
-          <div className='w-full absolute top-[30%] bg-[#5572af]/10 left-0 h-[500px] -skew-y-6'>
+        {/* Maps through the array of projects */}
+   
+          {projects?.map((project, i) => (
+            <div className=" w-screen flex-shrink-0 snap-center flex flex-col space-y-5 items-center justify-center p-20 md:p-44 h-screen">
+            <motion.img src={urlFor(project?.image).url()} alt="" />
+              <div className='space-y-10 px-0 md:px-10 max-w-6xl'>
+                <h4 className='text-4xl font-semibold text-center'>
+                  <span className='underline decoration-[#5572af]/50'>Case Study {i + 1} of {projects.length}: </span>
+                  {project?.title}
+                </h4>
               
-          </div>
-      </motion.div>
-  )
+                <div className='flex items-center space-x-7 justify-center'>
+                  {project.technologies.map((technology) => (
+                    <Image className='h-10 w-10 space-x-2'
+                      key={technology._id}
+                      src={urlFor(technology.image).url()}
+                      alt=""
+                      width="40"
+                      height="40"
+                    />
+                  ))}
+                </div>
+                <p className='text-lg text-center md:text-left'>{project.summary}</p>
+              </div>
+            </div>))}
+      </div>
+      <div className='w-full absolute top-[30%] bg-[#5572af]/10 left-0 h-[500px] -skew-y-6'>
+              
+      </div>
+  </motion.div></Slide>
+    )
 }
+
 export default Project;
