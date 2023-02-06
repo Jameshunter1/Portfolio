13a80e8ee0c58ce51b7a04befa21425badaf5689
[James Hunter] <Jameshunter1017@live.com>
2023-02-05 22:02:10 -0500

    Changed project images and adjusted chevrons, added source and live buttons

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -21,7 +21,7 @@ function Project({ projects }: Props) {
       whileInView={{ opacity: 1 }}
       transition={{ duration: 1.5 }}
       className='h-screen relative flex overflow-hidden flex-col text-left md:flex-row max-w-full justify-evenly mx-auto items-center z-0 '>
-      <h3 className='absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl'>
+      <h3 className='absolute top-20 uppercase tracking-[20px] text-gray-500 text-2xl mb-12'>
         Projects
       </h3>
      
@@ -33,11 +33,12 @@ function Project({ projects }: Props) {
     <section key={project._id} style={{ display: isCurrentProject ? 'block' : 'none' }}>
       <div className="w-screen flex-shrink-0 snap-center flex flex-col  items-center justify-center p-20 md:p-44 h-screen">
         <button 
-          className="absolute left-0 text-[50px] w-[200px] text-blue-500 " 
+          className="absolute left-0 text-[50px] w-[50px] text-blue-500 " 
           onClick={() => currentProjectIndex > 0 && setCurrentProjectIndex (currentProjectIndex - 1)}>&#10094;
         </button>
-        <motion.img src={urlFor(project?.image).url()} alt="" className='w:hidden w-[400px] mb-10'/>
-        <div className='space-y-10 px-0 md:px-10 max-w-6xl'>
+        <motion.img src={urlFor(project?.image).url()} alt="" className='w-[400px] md:w-[500px] h-[300px] md:h-[400px] lg:h-[600px] pt-12 md:pt-[120px] relative align-middle'/>
+        <button className="absolute right-0 text-[50px] w-[50px] text-blue-500" onClick={() => currentProjectIndex < projects.length - 1 && setCurrentProjectIndex(currentProjectIndex + 1)}>&#10095;</button>
+        <div className='space-y-10 px-0 md:px-10 max-w-6xl'> 
           <h4 className='text-xl md:text-3xl font-semibold text-center pt-10'>
             <span className='underline decoration-[#5572af]/50'>Project {i + 1} of {projects.length}: </span>
             {project?.title}
@@ -57,16 +58,18 @@ function Project({ projects }: Props) {
         
           
           <p className='text-lg text-center md:text-left hidden lg:block'>{project.summary}</p>
-
+<div className='flex justify-evenly'>
          <a href={project.url}>
-          <button  className="w-[200px] h-[200px]">
-          
-              
+          <button  className="w-[50px] md:w-[150px] h-[50px] bg-blue-700 text-white cursor-pointer rounded-full">Source
+           </button>
+            </a>
+            <a href={project.url}>
+          <button  className="w-[50px] md:w-[150px] h-[50px] bg-blue-700 text-white cursor-pointer rounded-full">Live
            </button>
           </a>
-
+</div>
         </div>
-        <button className="absolute right-0 text-[50px] w-[200px] text-blue-500" onClick={() => currentProjectIndex < projects.length-1 && setCurrentProjectIndex(currentProjectIndex + 1)}>&#10095;</button>
+       
       </div>
     </section>
   )
