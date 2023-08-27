0740f2b787ac24755b615fbdd9b7f6f73b2ca4a9
[James Hunter] <90528783+Jameshunter1@users.noreply.github.com>
2023-08-27 12:09:10 -0400

    Project section layout adjustments

--- components/ProjectCarousel.tsx
+++ components/ProjectCarousel.tsx
@@ -52,12 +52,12 @@ const [currentTab, setCurrentTab] = useState(
    
   return (
     <>
-      <div className="h-screen flex flex-col text-center items-center justify-center">
+      <div className="h-screen flex flex-col text-center justify-center items-center">
        <h3 className="uppercase tracking-[20px] text-gray-500 text-2xl text-center ">
         Projects
       </h3>
    
-         <div className="mt-[50px] flex">
+         <div className="mt-[50px] flex justify-center space-x-3">
       <button
         className={`px-4 py-2 rounded-full ${
           currentTab === Tab.Development ? "bg-blue-600 text-white" : "bg-gray-200 text-gray-600"
@@ -80,7 +80,8 @@ const [currentTab, setCurrentTab] = useState(
       {currentTab === Tab.Development && (
         // Development tab content goes here
      
-        <div className="flex  items-center justify-center">
+          <div className="flex  items-center justify-items-center">
+         
           <motion.button className="hover:bg-blue-700 text-white font-bold rounded-full focus:outline-none animate-slideLeft"
             onClick={prevProject}
           >
@@ -96,16 +97,17 @@ const [currentTab, setCurrentTab] = useState(
             >
               <path d="M15 18l-6-6 6-6" />
             </svg>
-          </motion.button>
-          <div className="justify-center mt-[40px]">
+              </motion.button>
+            
+         
               
-             <motion.div
+            <motion.div
           animate={{
             scale: [1, 2, 2, 1, 1],
             rotate: [0, 0, 270, 270, 0],
-            borderRadius: ["20%", "20%", "50%", "50%", "20%"],
+            
           }}
-          className="bg-center bg-no-repeat bg-contain "
+          className="bg-center bg-no-repeat bg-contain justify-center items-center"
           style={{
             backgroundImage: developmentProjects?.length > 0
               ? `url(${urlFor(developmentProjects[currentDevelopmentProject]?.image).url()})`
@@ -116,10 +118,10 @@ const [currentTab, setCurrentTab] = useState(
         />
 
 
-            <div className="flex justify-center p-5 md:p-4">
+            <div className=" p-5 md:p-4 ">
               {developmentProjects?.length > 0 && developmentProjects[currentDevelopmentProject]?.technologies.map((technology) => (
                 <Image
-                  className="rounded-sm cursor-pointer hover:opacity-80"
+                  className="rounded-sm cursor-pointer hover:opacity-80 bg-center bg-contain"
                   key={technology._id}
                   src={urlFor(technology.image).url()}
                   alt=""
@@ -128,10 +130,10 @@ const [currentTab, setCurrentTab] = useState(
                 />
               ))}
 
-            </div>
+          
             
               {/* {developmentProjects?.length > 0 && developmentProjects[currentDevelopmentProject]?.summary} */}
-              <div className="flex justify-evenly p-7">
+              <div className="flex justify-evenly p-7 space-x-2">
               <a href={developmentProjects[currentDevelopmentProject]?.url}>
                   <button className="w-[100px] h-[50px] text-white bg-gray-900 rounded-lg shadow-md hover:bg-gray-800 focus:outline-none">
                     Visit Site
@@ -174,8 +176,9 @@ const [currentTab, setCurrentTab] = useState(
         // Analysis tab content goes here
      
       
-        <div className="flex  items-center justify-evenly">
-          <motion.button className="hover:bg-blue-700 text-white font-bold rounded-full focus:outline-none animate-slideLeft "
+                <div className="flex items-center">
+
+              <motion.button className="hover:bg-blue-700 text-white font-bold rounded-full focus:outline-none animate-slideLeft "
             onClick={prevProject}
           >
             <svg
@@ -191,15 +194,14 @@ const [currentTab, setCurrentTab] = useState(
               <path d="M15 18l-6-6 6-6" />
             </svg>
           </motion.button>
-          <div className="justify-center  mt-[40px]">
               
   <motion.div
           animate={{
             scale: [1, 2, 2, 1, 1],
             rotate: [0, 0, 270, 270, 0],
-            borderRadius: ["20%", "20%", "50%", "50%", "20%"],
+            
           }}
-          className="bg-center bg-no-repeat justify-center"
+          className="bg-center bg-no-repeat bg-contain justify-center items-center"
           style={{
             backgroundImage: analysisProjects?.length > 0
               ? `url(${urlFor(analysisProjects[currentAnalysisProject]?.image).url()})`
@@ -211,10 +213,10 @@ const [currentTab, setCurrentTab] = useState(
 
 
 
-            <div className="flex justify-center p-2 md:p-4">
+            <div className="justify-center">
               {analysisProjects?.length > 0 && analysisProjects[currentAnalysisProject]?.technologies.map((technology) => (
                 <Image
-                  className="rounded-sm cursor-pointer hover:opacity-80"
+                  className="rounded-sm cursor-pointer hover:opacity-80 bg-center bg-cover"
                   key={technology._id}
                   src={urlFor(technology.image).url()}
                   alt=""
@@ -223,12 +225,12 @@ const [currentTab, setCurrentTab] = useState(
                 />
               ))}
 
-            </div>
+        
            
               {/* {analysisProjects?.length > 0 && analysisProjects[currentAnalysisProject].summary} */}
            
     
-           <div className="flex justify-evenly p-7">
+              <div className="flex justify-evenly p-7 space-x-2">
              <a href={analysisProjects[currentAnalysisProject]?.url}>
                   <button className="w-[100px] h-[50px] text-white bg-gray-900 rounded-lg shadow-md hover:bg-gray-800 focus:outline-none">
                     Visit Site
@@ -245,7 +247,7 @@ const [currentTab, setCurrentTab] = useState(
            
            
             <div className="flex  items-center justify-evenly">   
-             <button
+             <motion.button
             className="hover:bg-blue-700 text-white font-bold rounded-full focus:outline-none "
             onClick={nextProject}
           >
@@ -261,7 +263,7 @@ const [currentTab, setCurrentTab] = useState(
             >
               <path d="M9 18l6-6-6-6" />
             </svg>
-                  </button>
+                  </motion.button>
                   </div>
         
     </div>
